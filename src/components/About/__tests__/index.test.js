6fcf7a3c0c83d3d337fa37329ac37afcd215d69a
Your Name <irving.jem@gmail.com>
2022-06-05 09:25:44 -0700

    finished tests

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -3,6 +3,8 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
+const { asFragment } = render(<About />);
+
 describe('About component', () => {
     // First Test
     it('renders', () => {
@@ -10,6 +12,11 @@ describe('About component', () => {
     });
   
     // Second Test
+    it('matches snapshot DOM node structure', () => {
+        // render About
+      });
   })
 
+  expect(asFragment()).toMatchSnapshot();
+  
 afterEach(cleanup);
